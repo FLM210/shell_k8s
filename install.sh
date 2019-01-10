5e4d706a1849faad7caacd4b7a8411fb4bbfd20a
flm210 <zflmiy@qq.com>
2019-01-10 17:49:04 +0800

    fuck ansible_1

--- install.sh
+++ install.sh
@@ -39,40 +39,27 @@ done
 if [ "$ansible" == "yes" ];then
 	yum -y install ansible	&&  echo "ansible安装完成" 　||  echo "ansible安装失败请尝试手动安装"
 fi
+
 etcdnum=1 
 read -p "请输入集群安装方式（single/multi,allinone）:" type
 case $type in 
 single)
 	read -p "请输入master节点地址：" master
 	read -p "请输入时间服务器地址：" timeserver
-	read -p "请输入etcd节点地址文件路径：" etcd
-	read -p "请输入node节点地址文件路径：" node
+	read -p "请输入etcd节点地址(如有多个请用空格分开)：" etcd
+	read -p "请输入node节点地址(如有多个请用空格分开)：" node
 	read -p "请输入网络插件（calico, flannel, kube-router, cilium）：" network
 	sed  -i "s/192.168.1.1 NTP_ENABLED=no/$timeserver NTP_ENABLED=no/" /etc/ansible/example/hosts.s-master.example
 	sed  -i "s/^master/$master/" /etc/ansible/example/hosts.s-master.example
-	i=0
-	for j in `cat $etcd`
-	do
-        	etcdadd["${i}"]=$j
-        	let i++
-	done
-	l=$(($i))
-	for n in ${etcdadd[@]}
+	for n in $nodeadd
+    do
+        sed -i "$12a $n" /etc/ansible/example/hosts.s-master.example 
+    done
+　　 for n in $etcdadd
 	do
 		sed -i "8a $n "NODE_NAME=etcd${etcdnum}"" /etc/ansible/example/hosts.s-master.example 
 		etcdnum=$((${etcdnum}+1))
 	done
-        m=0
-        for n in `cat $node`
-        do
-                nodeadd["${m}"]=$n
-                let m++
-        done
-        for n in ${nodeadd[@]}
-        do
-                sed -i "$((12+${l}))a $n" /etc/ansible/example/hosts.s-master.example 
-        done
-
 	sed -i "s/CLUSTER_NETWORK=\"flannel\"/CLUSTER_NETWORK=\"$network\"/" /etc/ansible/example/hosts.s-master.example
 	\cp /etc/ansible/example/hosts.s-master.example /etc/ansible/hosts
 	echo "参数设置完成，开始安装集群。"
@@ -94,24 +81,12 @@ multi)
 	sed  -i "s/^master/$master_1 LB_IF=\"$mac\" LB_ROLE=master"/   /etc/ansible/example/hosts.m-masters.example
 	sed  -i "s/192.168.1.1 NTP_ENABLED=no/$timeserver NTP_ENABLED=no/" /etc/ansible/example/hosts.m-masters.example
 	sed  -i "10a $master_1" /etc/ansible/example/hosts.m-masters.example
-	sed  -i "11a $master_2" /etc/ansible/example/hosts.m-masters.example
-	i=0
-	for j in `cat $node`
-	do
-        	nodeadd["${i}"]=$j
-        	let i++
-	done  
-	for n in ${nodeadd[@]}
-        do
-                sed -i "17a $n" /etc/ansible/example/hosts.m-masters.example 
-        done
-	m=0
-	for n in `cat $etcd`
-	do
-        	etcdadd["${m}"]=$n
-        	let m++
-	done
-	for n in ${etcdadd[@]}
+	sed  -i "11a $master_2" /etc/ansible/example/hosts.m-masters.example 
+	for n in $nodeadd
+    do
+        sed -i "17a $n" /etc/ansible/example/hosts.m-masters.example 
+    done
+	for n in $etcdadd
 	do
 		sed -i "8a $n "NODE_NAME=etcd${etcdnum}"" /etc/ansible/example/hosts.m-masters.example 
 		etcdnum=$((${etcdnum}+1))
